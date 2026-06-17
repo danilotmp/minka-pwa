b6603b50d6e48ccd378a50f9fc4a1bbf7485f465
danilotmp <danilo.tmp@gmail.com>
2026-06-17 15:45:33 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-var CACHE_VERSION = 'v2';
 self.addEventListener('install', function() { self.skipWaiting(); });
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(names) {
-      return Promise.all(names.map(function(name) { return caches.delete(name); }));
-    }).then(function() { return clients.claim(); })
-  );
-});
+self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
 self.addEventListener('fetch', function(e) { e.respondWith(fetch(e.request)); });
